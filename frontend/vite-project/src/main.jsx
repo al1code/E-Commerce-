faf71bb8b4cfe8d3866562c111c6f4749cfd7fe1
Ali Kemal Dilek <alikemaldilek0@gmail.com>
2025-05-22 17:54:22 +0300

    Admin paneli tasarlandı

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,18 +1,25 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import CartProvider from "./context/CartProvider.jsx";
-import MainLayout from "./Layouts/MainLayout.jsx";
+import { Layout } from "./Layouts/Layouts.jsx";
 import App from "./App.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 
+
+
+
+
+console.log(window.location);
+
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
-      <MainLayout>
+      <Layout>
         <App />
-      </MainLayout>
+      </Layout>
     </CartProvider>
   </BrowserRouter>
 );
