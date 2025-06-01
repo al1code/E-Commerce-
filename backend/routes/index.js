55c03cfc57442f51db80770d1808fb85ad7467af
Rahmi Can <rahmi.can1517@gmail.com>
2025-06-01 15:50:39 +0300

    Ödeme işlemleri için kurulan Stripe'ın backend kısmı tamamlandı.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,13 +6,13 @@ const authRoute = require("./auth.js");
 const productRoute = require("./products.js");
 const couponRoute = require("./coupons.js");
 const userRoute = require("./users.js");
-
+const paymentRoute = require("./payment.js");
 
 router.use("/categories", categoryRoute);
 router.use("/auth", authRoute);
 router.use("/products", productRoute);
 router.use("/coupons", couponRoute);
 router.use("/users", userRoute);
-
+router.use("/payment", paymentRoute);
 
 module.exports = router;
